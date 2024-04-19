c6f8bc567747a3e04c9845c10480c55f97d48795
Flavio-Portela <fmspx@hotmail.com>
2024-04-19 00:18:36 -0300

    [release/1.0.0] news changes

--- frameworks/spring/littlecode-jpa/src/main/java/com/littlecode/jpa/datasource/DSFactory.java
+++ frameworks/spring/littlecode-jpa/src/main/java/com/littlecode/jpa/datasource/DSFactory.java
@@ -2,6 +2,7 @@ package com.littlecode.jpa.datasource;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.cfg.AvailableSettings;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -14,7 +15,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 import javax.sql.DataSource;
 import java.util.Map;
 
-@RequiredArgsConstructor
+@Slf4j
 public class DSFactory{
     private final Environment environment;
     @Getter
@@ -22,42 +23,100 @@ public class DSFactory{
     @Getter
     private final String[] packages;
 
+    public DSFactory(Environment environment, Database database, String[] packages) {
+        this.environment = environment;
+        this.database = database;
+        this.packages = packages;
+        log.info("Using database {}", database);
+        log.info("Using packages {}", packages);
+    }
+
     public HibernateJpaVendorAdapter makeVendorAdapter() {
-        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        vendorAdapter.setDatabase(getDatabase());
-        return vendorAdapter;
+        internalLog("   makeVendorAdapter","started");
+        internalLog("       database",getDatabase()==null?"undefined":getDatabase().name());
+        try{
+            HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+            vendorAdapter.setDatabase(getDatabase());
+            return vendorAdapter;
+        }
+        finally {
+            internalLog("   makeVendorAdapter","finished");
+        }
     }
 
     public LocalContainerEntityManagerFactoryBean makeEntityManagerFactory() {
-        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-        em.setDataSource(makeDataSource());
-        em.setPackagesToScan(getPackages());
-        em.setJpaVendorAdapter(makeVendorAdapter());
+        internalLog("makeEntityManagerFactory","started");
+        try {
+            LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+            em.setDataSource(makeDataSource());
+            em.setPackagesToScan(getPackages());
+            em.setJpaVendorAdapter(makeVendorAdapter());
 
-        Map<String, Object> properties = Map.of(
-                AvailableSettings.PHYSICAL_NAMING_STRATEGY, org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy.class.getCanonicalName(),
-                AvailableSettings.IMPLICIT_NAMING_STRATEGY, org.hibernate.boot.model.naming.ImplicitNamingStrategyLegacyHbmImpl.class.getCanonicalName()
-        );
+            Map<String, Object> properties = Map.of(
+                    AvailableSettings.PHYSICAL_NAMING_STRATEGY, org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy.class.getCanonicalName(),
+                    AvailableSettings.IMPLICIT_NAMING_STRATEGY, org.hibernate.boot.model.naming.ImplicitNamingStrategyLegacyHbmImpl.class.getCanonicalName()
+            );
 
-        em.setJpaPropertyMap(properties);
-        return em;
+            em.setJpaPropertyMap(properties);
+            return em;
+        }finally {
+            internalLog("makeEntityManagerFactory","finished", packages);
+        }
     }
 
     public DataSource makeDataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        if(this.getDatabase()!=null){
-            var __dbType=this.getDatabase().name().toLowerCase();
-            dataSource.setUrl(environment.getProperty(String.format("spring.datasource.%s.url",__dbType)));
-            dataSource.setUsername(environment.getProperty(String.format("spring.datasource.%s.username",__dbType)));
-            dataSource.setPassword(environment.getProperty(String.format("spring.datasource.%s.password",__dbType)));
-            dataSource.setSchema(environment.getProperty(String.format("spring.datasource.%s.schema",__dbType)));
+        internalLog("   makeDataSource","started");
+        try{
+            DriverManagerDataSource dataSource = new DriverManagerDataSource();
+            if(this.getDatabase()==null)
+                internalLog("makeDataSource","database is null");
+            else{
+
+                var __env_dbType=this.getDatabase().name().toLowerCase();
+                var __env_url=String.format("spring.datasource.%s.url",__env_dbType);
+                var __env_username=String.format("spring.datasource.%s.username",__env_dbType);
+                var __env_password=String.format("spring.datasource.%s.password",__env_dbType);
+                var __env_schema=String.format("spring.datasource.%s.schema",__env_dbType);
+
+                internalLog("       env","");
+                internalLog("           ","env.dbType",__env_dbType);
+                internalLog("           ","env.url",__env_url);
+                internalLog("           ","env.username",__env_username);
+                internalLog("           ","env.password",__env_password);
+                internalLog("           ","env.schema",__env_schema);
+
+                var __dbType=this.getDatabase().name().toLowerCase();
+                var __url=environment.getProperty(String.format("spring.datasource.%s.url",__env_dbType));
+                var __username=environment.getProperty(String.format("spring.datasource.%s.username",__env_dbType));
+                var __password=environment.getProperty(String.format("spring.datasource.%s.password",__env_dbType));
+                var __schema=environment.getProperty(String.format("spring.datasource.%s.schema",__env_dbType));
+
+                internalLog("       value","");
+                internalLog("           ","value.dbType ",__dbType);
+                internalLog("           ","value.url", __url);
+                internalLog("           ","value.username",__username);
+                internalLog("           ","value.password",__password);
+                internalLog("           ","value.schema",__schema);
+
+                dataSource.setUrl(__url);
+                dataSource.setUsername(__username);
+                dataSource.setPassword(__password);
+                dataSource.setSchema(__schema);
+            }
+            return dataSource;
+
+        }finally {
+            internalLog("   makeDataSource","finished", packages);
         }
-        return dataSource;
     }
 
     public PlatformTransactionManager makeTransactionManager() {
         JpaTransactionManager transactionManager = new JpaTransactionManager();
         transactionManager.setEntityManagerFactory(makeEntityManagerFactory().getObject());
         return transactionManager;
+    }
+
+    private static void internalLog(String method, String format, Object... args){
+        log.info(("{} - "+format+": {}"),  method, args);
     }
 }
