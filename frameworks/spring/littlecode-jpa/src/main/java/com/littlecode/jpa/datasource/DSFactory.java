21ccbfaf17d202a1411ff7f36e272403470b2da7
Flavio-Portela <fmspx@hotmail.com>
2024-04-22 20:40:57 -0300

    [release/1.0.0] news changes

--- frameworks/spring/littlecode-jpa/src/main/java/com/littlecode/jpa/datasource/DSFactory.java
+++ frameworks/spring/littlecode-jpa/src/main/java/com/littlecode/jpa/datasource/DSFactory.java
@@ -43,10 +43,10 @@ public class DSFactory {
 
     public LocalContainerEntityManagerFactoryBean makeEntityManagerFactory() {
         LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-        em.setDataSource(makeDataSource());
-        em.setPackagesToScan(getPackages());
         em.setJpaVendorAdapter(makeVendorAdapter());
-        em.setJpaPropertyMap(__make_default_properties());
+        em.setPackagesToScan(getPackages());
+        em.setDataSource(makeDataSource());
+//        em.setJpaPropertyMap(__make_default_properties());
         em.afterPropertiesSet();
         return em;
     }
@@ -90,19 +90,19 @@ public class DSFactory {
         return __ds_value==null?"":__ds_value;
     }
 
-    private Map<String,String> __make_default_properties() {
-        Map<String,String> properties=new HashMap<>();
-        var hibernate_envs= List.of(AvailableSettings.DIALECT,AvailableSettings.SHOW_SQL,AvailableSettings.FORMAT_SQL,AvailableSettings.USE_SQL_COMMENTS,AvailableSettings.PHYSICAL_NAMING_STRATEGY,AvailableSettings.IMPLICIT_NAMING_STRATEGY);
-        for(var env: hibernate_envs)
-            __property__add(properties, env);
-
-        if(!properties.containsKey(AvailableSettings.PHYSICAL_NAMING_STRATEGY))
-            __property__add(properties, AvailableSettings.PHYSICAL_NAMING_STRATEGY, CamelCaseToUnderscoresNamingStrategy.class.getCanonicalName());
-        if(!properties.containsKey(AvailableSettings.IMPLICIT_NAMING_STRATEGY))
-            __property__add(properties, AvailableSettings.IMPLICIT_NAMING_STRATEGY, SpringImplicitNamingStrategy.class.getCanonicalName());
-
-        //PhysicalNamingStrategyStandardImpl.INSTANCE.
-
-        return properties;
-    }
+//    private Map<String,String> __make_default_properties() {
+//        Map<String,String> properties=new HashMap<>();
+//        var hibernate_envs= List.of(AvailableSettings.DIALECT,AvailableSettings.SHOW_SQL,AvailableSettings.FORMAT_SQL,AvailableSettings.USE_SQL_COMMENTS,AvailableSettings.PHYSICAL_NAMING_STRATEGY,AvailableSettings.IMPLICIT_NAMING_STRATEGY);
+//        for(var env: hibernate_envs)
+//            __property__add(properties, env);
+//
+//        if(!properties.containsKey(AvailableSettings.PHYSICAL_NAMING_STRATEGY))
+//            __property__add(properties, AvailableSettings.PHYSICAL_NAMING_STRATEGY, CamelCaseToUnderscoresNamingStrategy.class.getCanonicalName());
+//        if(!properties.containsKey(AvailableSettings.IMPLICIT_NAMING_STRATEGY))
+//            __property__add(properties, AvailableSettings.IMPLICIT_NAMING_STRATEGY, SpringImplicitNamingStrategy.class.getCanonicalName());
+//
+//        //PhysicalNamingStrategyStandardImpl.INSTANCE.
+//
+//        return properties;
+//    }
 }
